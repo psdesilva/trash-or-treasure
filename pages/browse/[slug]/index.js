95b6b89888d4a8c50834c7ca76b6591206d0a6ce
Priyanka de Silva <cdomicolo@oswego.edu>
2021-10-21 14:01:05 +0530

    fixed broken links for nonexisten items

--- pages/browse/[slug]/index.js
+++ pages/browse/[slug]/index.js
@@ -1,8 +1,7 @@
 import Layout from '../../../components/Layout'
 import NestedLayout from '../../../components/NestedLayout'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import itemStyle from '../../../styles/Item.module.css'
 import { useItems } from '../../../components/ItemContext'
@@ -23,6 +22,13 @@ const Item = ({ slug }) => {
     const { user, error, isLoading } = useUser();
     const [showDelete, setShowDelete] = useState(false);
     const [itemToDelete, setItemToDelete] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      if (item) {
+        setLoading(false)
+      }
+    },[item])
 
     function goBack (e) {
       e.preventDefault();
@@ -58,7 +64,18 @@ const Item = ({ slug }) => {
       }
     }
 
-    return (
+    if (loading == true) {
+      return (
+        <div className={itemStyle.container}>
+          <div className={itemStyle.back}>
+            <a href="#" onClick={goBack}><FiArrowLeftCircle /></a>
+          </div>
+          <h1>Item not found.</h1>
+        </div>
+        
+      )
+    } else {
+          return (
         <div className={itemStyle.container}>
             <div className={itemStyle.back}>
               <a href="#" onClick={goBack}><FiArrowLeftCircle /></a>
@@ -106,6 +123,56 @@ const Item = ({ slug }) => {
             <DeleteConfirmationModal setItemToDelete={setItemToDelete} itemToDelete={itemToDelete} showDelete={showDelete} onClose={() => setShowDelete(false)}/>
         </div>
     )
+    }
+
+    // return (
+    //     <div className={itemStyle.container}>
+    //         <div className={itemStyle.back}>
+    //           <a href="#" onClick={goBack}><FiArrowLeftCircle /></a>
+    //           {user ? user.sub === item.user ? <button onClick={() => deleteConfirm(item.id)}><MdDeleteForever /></button> : '' : ''}
+    //         </div>
+    //         <div className={itemStyle.itemDetails}>
+    //         { isBreakPoint ? <div className={itemStyle.name}><h1>{item.name}</h1></div> : ''}
+    //           <div className={itemStyle.imageContainer}>
+    //             <Image 
+    //               src={item.img}
+    //               layout="fill"
+    //               objectFit="cover"
+    //               className={itemStyle.image}
+    //             /> 
+    //           </div>
+    //           <div className={itemStyle.info}>
+    //             { isBreakPoint ? '' : <div className={itemStyle.name}><h1>{item.name}</h1></div>}
+    //             <div className={itemStyle.infoText}>
+    //               <div className={itemStyle.individualInfo}>
+    //                 <div className={itemStyle.individualInfoIcon}>
+    //                   <MdLocationOn />
+    //                 </div>
+    //                 <p><span className={itemStyle.bold}>Location:&#160;</span> {item.location}</p>
+    //               </div>
+    //               <div className={itemStyle.individualInfo}>
+    //                 <div className={itemStyle.individualInfoIcon}>
+    //                   <MdPhoneIphone />
+    //                 </div>
+    //                 {generateContact(item.contact, <p className={itemStyle.contact}><span className={itemStyle.bold}>{Object.entries(item.contact)[0][0]}:&#160;</span> {Object.entries(item.contact)[0][1]}</p>)}
+    //                 {/* <p><span className={itemStyle.bold}>{Object.entries(item.contact)[0][0]}:&#160;</span> {Object.entries(item.contact)[0][1]}</p> */}
+    //               </div>
+    //               <div className={itemStyle.individualInfo}>
+    //                 <div className={itemStyle.individualInfoIcon}>
+    //                   <MdDescription />
+    //                 </div>
+    //                 <p><span className={itemStyle.bold}>Description:&#160;</span> {item.description}</p>
+    //               </div>
+    //               <div className={itemStyle.condition}>
+    //                 <p>{item.used}</p>
+    //                 <p>{item.broken}</p>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <DeleteConfirmationModal setItemToDelete={setItemToDelete} itemToDelete={itemToDelete} showDelete={showDelete} onClose={() => setShowDelete(false)}/>
+    //     </div>
+    // )
 }
 
 export const getServerSideProps = async (context) => {
