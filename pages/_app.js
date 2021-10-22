3b73fce7a06c4028ce65b4ede7e388abf6e0124a
Priyanka de Silva <cdomicolo@oswego.edu>
2021-10-22 19:15:45 +0530

    added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,16 @@ import Layout from '../components/Layout'
 import { ItemContextProvider } from '../components/ItemContext'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import '../styles/globals.css'
+import "../styles/nprogress.css";
 import SimpleReactLightbox from 'simple-react-lightbox'
+import NProgress from "nprogress";
+import Router from 'next/router'
+
+Router.events.on("routeChangeStart", NProgress.start);
+Router.events.on("routeChangeError", NProgress.done);
+Router.events.on("routeChangeComplete", NProgress.done);
+
+NProgress.configure({ showSpinner: false })
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
