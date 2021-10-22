eac186d707f0e8689d5ab0a2f87eb89dca4e0e47
Priyanka de Silva <cdomicolo@oswego.edu>
2021-10-22 22:25:06 +0530

    changed landing page style

--- components/LandingPageDisplay.js
+++ components/LandingPageDisplay.js
@@ -5,9 +5,10 @@ const LandingPageDisplay = () => {
         <div className={LandingPageStyles.landingPageDisplay}>
             <div className={LandingPageStyles.overlay}></div>
             <div className={LandingPageStyles.landingPageDisplayText}>
-                <h1 className={LandingPageStyles.heading}>Trash or Treasure</h1>
-                <p>{`Moving? Spring cleaning? Just got too much stuff? Don't throw it away, someone out there might be looking for it!`}</p>
-                <p>{`One man's trash is another man's treasure.`}</p>
+                <h1 className={LandingPageStyles.heading}>Trash <span className={LandingPageStyles.or}>or</span> Treasure</h1>
+                <div className={LandingPageStyles.separator}></div>
+                <p>Moving? Spring cleaning? Just got too much stuff?<br/> Don't throw it away, someone out there might be looking for it!</p>
+                <p>One man's trash is another man's treasure.</p>
             </div>
         </div>
     )
